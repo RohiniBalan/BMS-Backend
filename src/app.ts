483d8d8982c73383e3d54d5e43f3780e21150d9d
RohiniBalan <rohinibalan529@gmail.com>
2026-06-02 16:40:22 +0530

    updated changes

--- src/app.ts
+++ src/app.ts
@@ -25,12 +25,7 @@ const app = express();
 // Core middleware
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(
